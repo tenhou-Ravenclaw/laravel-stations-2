6ccb1865a5b879fca09209c228a0d8202994be39
tenhou-Ravenclaw <tenhou.0126@gmail.com>
2025-05-22 21:11:52 +0900

    station6?

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -12,6 +12,9 @@
         <li>ID: {{ $movie->id }}</li>
         <li>映画タイトル: {{ $movie->title }}</li>
         <li>画像URL: {{ $movie->image_url }}</li>
+        <td>{{ $movie->published_year }}</td>
+        <td>{{ $movie->is_showing ? '上映中' : '上映終了' }}</td>
+        <td>{{ $movie->description }}</td>
         <li>登録日時: {{ $movie->created_at }}</li>
         <li>更新日時: {{ $movie->updated_at }}</li>
     @endforeach
